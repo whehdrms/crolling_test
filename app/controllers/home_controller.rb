4ddcb269b352613cc808fb21faa8d2ef5de62206
whehdrms <d1g1mong@naver.com>
2017-02-24 14:37:04 +0000

    hi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 class HomeController < ApplicationController
   def index
-    
+
     @title = Array.new
-    
-      0.upto(1) do |d|
+
+      0.upto(30) do |d|
       @doc = Nokogiri::HTML(open("http://ndhs.or.kr/2014/sub_promotion/sub3.php?menuidx=49&bname=photo&category=&page=#{d}&search=&key="))
       @doc.css('img').each do |p|
         if p['src'].include?('253x153_99')
